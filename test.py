220220c2df1c38d7400f24eeddd289838af72ee5
Muhammad-Shah <msh87376@gmail.com>
2024-06-25 18:54:11 +0500

    test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 # write streamlit code for mcq with 4 options
+def write_mcq(mcq_statement, mcq_option1, mcq_option2, mcq_option3, mcq_option4):
+    st.write(mcq_statement)
+    st.write(mcq_option1)
+    st.write(mcq_option2)
+    st.write(mcq_option3)
+    st.write(mcq_option4)
